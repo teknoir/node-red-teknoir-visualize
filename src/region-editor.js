a131e70492dec9cc19d86a8c98a55a7c12da755a
Anders Åslund <anders.aslund@teknoir.ai>
2022-09-05 09:33:55 +0200

    Region editor, first fully working version

--- src/region-editor.js
+++ src/region-editor.js
@@ -10,11 +10,13 @@
 //     };
 // }();
 
-module.exports = function init(RED) {
+module.exports = function(RED) {
+    "use strict";
 
-    function RegionEditor(config) {
+    function RegionEditor(n) {
+        RED.nodes.createNode(this,n);
         let node = this;
-        this.json = config.json || '{}';
+        let json = n.json || '{}';
 
         function sendDataToClient(data, msg) {
             let d = {
@@ -24,22 +26,58 @@ module.exports = function init(RED) {
                 d.data = data;
             }
             try {
-                RED.comms.publish("graph-plot", d);
+                RED.comms.publish("region-editor", d);
             } catch (e) {
                 node.error("Error sending data", msg);
             }
         }
 
-        node.on("input", function (msg) {
-            node.status({fill: "green", shape: "dot", text: ""});
+        this.on("input", function(msg, send, done) {
+            // TODO: Validate payload is mime image
             sendDataToClient(msg.payload, msg);
-        });
-
-        node.on("close", function () {
-            // RED.comms.publish("data", {id: this.id});
-            // node.status({});
+            msg.payload = {
+                image: msg.payload,
+                polygons: JSON.parse(json)
+            }
+            send(msg);
+            done();
         });
     }
 
-    RED.nodes.registerType('region-editor', RegionEditor);
-};
+    RED.nodes.registerType("region-editor",RegionEditor);
+}
+
+// module.exports = function init(RED) {
+//
+//     function RegionEditor(config) {
+//         let node = this;
+//         this.json = config.json || '{}';
+//
+//         function sendDataToClient(data, msg) {
+//             let d = {
+//                 id: node.id,
+//             };
+//             if (data) {
+//                 d.data = data;
+//             }
+//             try {
+//                 RED.comms.publish("graph-plot", d);
+//             } catch (e) {
+//                 node.error("Error sending data", msg);
+//             }
+//         }
+//
+//         node.on("input", function (msg) {
+//             node.status({fill: "green", shape: "dot", text: ""});
+//             node.error("Error sending data", msg);
+//             sendDataToClient(msg.payload, msg);
+//         });
+//
+//         node.on("close", function () {
+//             // RED.comms.publish("data", {id: this.id});
+//             // node.status({});
+//         });
+//     }
+//
+//     RED.nodes.registerType('region-editor', RegionEditor);
+// };
